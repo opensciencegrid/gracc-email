16ded31708021915cc8b7e7986ac59e03c3a1845
Derek Weitzel <djw8605@gmail.com>
2017-08-08 16:54:26 -0500

    Adding more processing

--- src/backup_report.py
+++ src/backup_report.py
@@ -11,29 +11,43 @@ import argparse
 import stat
 import toml
 
-def parse_dir(remote_path):
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
+
+
+def parse_dir(remote_path, how_old=7):
     "Parse a single directory"
     context = gfal2.creat_context()
-    
-    file = "gsiftp://red-gridftp3.unl.edu/user/hcc/gracc_job_backups"
-    st = context.stat(remote_path)
-    print st
+    to_return = {}
+    to_return['total_size'] = 0
+    to_return['total_files'] = 0
+    to_return['old_total_size'] = 0
+    to_return['old_total_files'] = 0
+   
+    st = context.stat(str(remote_path))
     if not stat.S_ISDIR(st.st_mode):
         raise Exception("Remote file %s is not a directory" % remote_path)
     
-    directory = context.opendir(remote_path)
+    directory = context.opendir(str(remote_path))
     st = None
     
     while True:
         (dirent, st) = directory.readpp()
         if dirent is None or dirent.d_name is None or len(dirent.d_name) == 0:
             break
-        print dirent
-        print dirent.d_name
-        print st
-    
-    
-
+        mtime = datetime.datetime.fromtimestamp(int(st.st_mtime))
+        if mtime > (datetime.datetime.now() - datetime.timedelta(days=how_old)):
+            to_return['total_size'] += st.st_size
+            to_return['total_files'] += 1
+        if mtime < (datetime.datetime.now() - datetime.timedelta(days=how_old)) and mtime > (datetime.datetime.now() - datetime.timedelta(days=how_old*2)):
+            to_return['old_total_size'] += st.st_size
+            to_return['old_total_files'] += 1
+   
+    return to_return
 
 
 def add_args(parser):
@@ -62,8 +76,9 @@ def main():
         raise Exception("Directories are not listed in the config file %s" % args.config)
     
     for directory in config['directories']:
-        parse_dir(directory)
-    
+        results = parse_dir(directory)
+        print "For directory %s: %s, %i files" % (directory, sizeof_fmt(results['total_size']), results['total_files'])
+        print "Old: %s, %i" % (sizeof_fmt(results['old_total_size']), results['old_total_files'])
 
 
 if __name__ == "__main__":
