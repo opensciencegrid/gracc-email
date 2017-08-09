806249317df75c242c12157d24010a8f5b64b6ca
Derek Weitzel <djw8605@gmail.com>
2017-08-09 10:44:37 -0500

    Adding email sending capabilities

--- src/backup_report.py
+++ src/backup_report.py
@@ -27,7 +27,8 @@ def parse_dir(remote_path, how_old=7):
     to_return['total_files'] = 0
     to_return['old_total_size'] = 0
     to_return['old_total_files'] = 0
-   
+    to_return['files'] = []
+    files = []
     st = context.stat(str(remote_path))
     if not stat.S_ISDIR(st.st_mode):
         raise Exception("Remote file %s is not a directory" % remote_path)
@@ -43,10 +44,12 @@ def parse_dir(remote_path, how_old=7):
         if mtime > (datetime.datetime.now() - datetime.timedelta(days=how_old)):
             to_return['total_size'] += st.st_size
             to_return['total_files'] += 1
+            files.append({'filename': dirent.d_name, 'size': st.st_size})
         if mtime < (datetime.datetime.now() - datetime.timedelta(days=how_old)) and mtime > (datetime.datetime.now() - datetime.timedelta(days=how_old*2)):
             to_return['old_total_size'] += st.st_size
             to_return['old_total_files'] += 1
-   
+  
+    to_return['files'] = files
     return to_return
 
 
@@ -74,11 +77,60 @@ def main():
     config = toml.load(args.config)
     if not "directories" in config:
         raise Exception("Directories are not listed in the config file %s" % args.config)
-    
+
+    summary_text = ""
+    detailed_text = []
+    headers = ["Directory", "Bytes in Last Week", 'Delta from Last Week']
+#    toReturn +=  "Records by Probe:\n"
+
+    data = []
+    total_backed_up = 0
+    detailed_files = ""
     for directory in config['directories']:
         results = parse_dir(directory)
-        print "For directory %s: %s, %i files" % (directory, sizeof_fmt(results['total_size']), results['total_files'])
-        print "Old: %s, %i" % (sizeof_fmt(results['old_total_size']), results['old_total_files'])
+        data.append([directory, sizeof_fmt(results['total_size']), "%.2f%%" % ((float(results['total_size'] - results['old_total_size']) / max(results['total_size'], 1)) * 100)])
+        total_backed_up += results['total_size']
+        detailed_files += "Directory: %s\n" % directory
+        file_header = ["Filename", "Size"]
+        single_file_data = []
+        for single_file in results['files']:
+            single_file_data.append([ single_file['filename'], sizeof_fmt(single_file['size']) ])
+
+        detailed_files += tabulate(single_file_data, headers=file_header, tablefmt='grid')
+        detailed_files += "\n\n"
+
+    email_body = ""
+    email_body += tabulate(data, headers=headers, tablefmt='grid')
+
+    # A divider     
+    email_body += "\n" + ("-" * 75) + "\n"
+
+    email_body += detailed_files
+
+    subject = "GRACC Backups - %s bytes backed up last week" % sizeof_fmt(total_backed_up)
+
+    # A divider
+    email_body += "\n" + ("-" * 75) + "\n"
+
+    # Calculate and report the time it took to generate the email
+    after = datetime.datetime.now()
+    difference = after - before
+    email_body += "\n\n"
+    email_body += "Took %i seconds to generate email." % (difference.seconds)
+
+    # Create the email
+    msg = MIMEText("<pre style=\"font-size:small\">" + email_body + "</pre>", 'html')
+    msg['Subject'] = subject
+    msg['From'] = "GRACC-Collector"
+    s = smtplib.SMTP('localhost')
+
+    for email in args.emails:
+        msg['To'] = email
+        s.sendmail("GRACC-Collector", [email], msg.as_string())
+
+    s.quit()
+
+
 
 
 if __name__ == "__main__":
